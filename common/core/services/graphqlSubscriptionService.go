73a17368e50f5d1ea266fc418e9459ed8fd864b2
geoff-nunan <39071789+geoff-nunan@users.noreply.github.com>
2021-07-20 14:05:32 +1000

    added NATS and GraphQL Subscription drivers

---
+++ common/core/services/graphqlSubscriptionService.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"github.com/Spruik/libre-common/common/core/domain"
+	"github.com/Spruik/libre-common/common/core/ports"
+	"time"
+)
+
+type graphqlSubscriptionService struct {
+	port ports.EdgeConnectorPort
+}
+
+func NewGraphqlSubscriptionService(port ports.EdgeConnectorPort) *graphqlSubscriptionService {
+	return &graphqlSubscriptionService{
+		port: port,
+	}
+}
+
+var graphqlSubscriptionServiceInstance *graphqlSubscriptionService = nil
+
+func SetGraphqlSubscriptionServiceInstance(inst *graphqlSubscriptionService) {
+	graphqlSubscriptionServiceInstance = inst
+}
+func GetGraphqlSubscriptionServiceInstance() *graphqlSubscriptionService {
+	return graphqlSubscriptionServiceInstance
+}
+
+func (s *graphqlSubscriptionService) Connect(connInfo map[string]interface{}) error {
+	return s.port.Connect(connInfo)
+}
+func (s *graphqlSubscriptionService) Close() error {
+	return s.port.Close()
+}
+func (s *graphqlSubscriptionService) ReadTags(inTagDefs []domain.StdMessageStruct) []domain.StdMessageStruct {
+	return s.port.ReadTags(inTagDefs)
+}
+func (s *graphqlSubscriptionService) WriteTags(outTagDefs []domain.StdMessageStruct) []domain.StdMessageStruct {
+	return s.port.WriteTags(outTagDefs)
+}
+func (s *graphqlSubscriptionService) ListenForEdgeTagChanges(c chan domain.StdMessageStruct, changeFilter map[string]interface{}) {
+	s.port.ListenForEdgeTagChanges(c, changeFilter)
+}
+func (s *graphqlSubscriptionService) GetTagHistory(startTS time.Time, endTS time.Time, inTagDefs []domain.StdMessageStruct) []domain.StdMessageStruct {
+	return s.port.GetTagHistory(startTS, endTS, inTagDefs)
+}
--- go.mod
+++ go.mod
@@ -7,9 +7,14 @@ require (
 	github.com/Spruik/libre-configuration v0.1.0
 	github.com/Spruik/libre-logging v0.1.0
 	github.com/eclipse/paho.golang v0.9.0
+	github.com/google/uuid v1.1.2
 	github.com/gopcua/opcua v0.1.13
 	github.com/gorilla/mux v1.8.0
 	github.com/hasura/go-graphql-client v0.2.0
 	github.com/influxdata/influxdb-client-go/v2 v2.4.0
+	github.com/nats-io/nats-server/v2 v2.3.2 // indirect
+	github.com/nats-io/nats.go v1.11.1-0.20210623165838-4b75fc59ae30
+	google.golang.org/protobuf v1.27.1 // indirect
+	nhooyr.io/websocket v1.8.6
 
 )
