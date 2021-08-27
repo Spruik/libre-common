f106ef70a8766b31160d022b344dd342d1be548e
Tom Hollingworth <i8pizza2@gmail.com>
2021-08-27 13:23:16 -0400

    Remove size initialization from 0 sized array

--- common/utilities/equipmentFinderDefault.go
+++ common/utilities/equipmentFinderDefault.go
@@ -8,7 +8,6 @@ import (
 	"github.com/Spruik/libre-common/common/core/domain"
 	"github.com/Spruik/libre-common/common/core/ports"
 	"github.com/Spruik/libre-common/common/core/queries"
-	"github.com/Spruik/libre-configuration"
 	libreConfig "github.com/Spruik/libre-configuration"
 	libreLogger "github.com/Spruik/libre-logging"
 )
@@ -150,7 +149,7 @@ func (s *equipmentFinderDefault) SubscribeToChanges(notificationChannel chan por
 func (s *equipmentFinderDefault) getQueryInput(txn ports.LibreDataStoreTransactionPort) ([]domain.EquipmentElementLevel, []string, []string, error) {
 	var err error
 	//get the equipment type list from config
-	eqElLevels := make([]domain.EquipmentElementLevel, 0, 0)
+	eqElLevels := make([]domain.EquipmentElementLevel, 0)
 	var eqElLevel domain.EquipmentElementLevel
 	levelList, _ := s.GetConfigItemWithDefault("ACTIVE_EQ_LEVELS", "")
 	if levelList != "" {
@@ -163,7 +162,7 @@ func (s *equipmentFinderDefault) getQueryInput(txn ports.LibreDataStoreTransacti
 
 	//get the includes from config
 	incList, _ := s.GetConfigItemWithDefault("INCLUDE_EQUIPMENT", "")
-	includeIds := make([]string, 0, 0)
+	includeIds := make([]string, 0)
 	if incList != "" {
 		incSlice := strings.Split(incList, ",")
 		for _, eqName := range incSlice {
@@ -178,7 +177,7 @@ func (s *equipmentFinderDefault) getQueryInput(txn ports.LibreDataStoreTransacti
 
 	//get the excludes from config
 	excList, _ := s.GetConfigItemWithDefault("EXCLUDE_EQUIPMENT", "")
-	excludeIds := make([]string, 0, 0)
+	excludeIds := make([]string, 0)
 	if excList != "" {
 		excSlice := strings.Split(excList, ",")
 		for _, eqName := range excSlice {
